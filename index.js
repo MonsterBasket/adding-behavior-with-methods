2f71e8c1ff93f5047b6c0d7f6fc984a507b273ab
MonsterBasket <blazza1@hotmail.com>
2022-07-29 15:24:01 +1000

    test complete - not sure about those methods though

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Animal{
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+    }
+    speak(message){
+        return message;
+    }
+}
+class Cat extends Animal{
+    constructor(name, gender){
+        super(name, gender);
+        this.message = `${name} says meow!`;
+    }
+    speak(){
+        return super.speak(this.message);
+    }
+}
+class Dog extends Animal{
+    constructor(name, gender){
+        super(name, gender);
+        this.message = `${name} says woof!`;
+    }
+    speak(){
+        return super.speak(this.message);
+    }
+}
+class Bird extends Animal{
+    constructor(name, gender){
+        super(name, gender);
+        if (this.gender === "male")
+        this.message = `It's me! ${this.name}, the parrot!`;
+        else
+        this.message = `${this.name} says squawk!`;
+    }
+    speak(){
+        return super.speak(this.message);
+    }
+}
+const cat = new Cat("Sasha", "female")
+const dog = new Dog("Rufio", "male")
+const bird2 = new Bird("Mable", "female")
+const bird = new Bird("Pablo", "male")
